446075475eb9f6316fa7df6875d3e213c32c8fe6
arne <arne@mw171175.med.rug.nl>
2016-02-15 11:07:48 +0100

    trypsin replaced with previous non regex method

--- src/peptidedigesters/TrypsinDigester.java
+++ src/peptidedigesters/TrypsinDigester.java
@@ -8,8 +8,6 @@ package peptidedigesters;
 import peptidecutter.PeptideCutter;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.regex.Pattern;
-import peptidematcher.PeptideMatcher;
 
 /**
  *
@@ -21,16 +19,6 @@ public class TrypsinDigester implements Digester {
      * contains the minimal length a peptide should have.
      */
     private final Integer minimalLength;
-    /**
-     * The first pattern of the Pepsin Low PH digester. pattern created by
-     * Rutger Ozinga
-     */
-    private final Pattern pattern1 = Pattern.compile("(?<=W)[K](?=P)");
-    /**
-     * The second pattern of the Pepsin Low PH digester. pattern created by
-     * Rutger Ozinga
-     */
-    private final Pattern pattern2 = Pattern.compile("(?<=M)[R](?=P)");
 
     /**
      * Initiates the class.
@@ -46,10 +34,37 @@ public class TrypsinDigester implements Digester {
         // Trypsin
         // The order for the sites is:
         // p4   p3  p2  p1  p1F p2F p3F p4F
+        ArrayList<Character> aa = new ArrayList<>(Arrays.asList('K', 'R'));
         ArrayList<Integer> indices = new ArrayList<>(Arrays.asList(-1, peptide.length() - 1));
-        PeptideMatcher pm = new PeptideMatcher();
-        indices.addAll(pm.getIndexList(pattern1, peptide));
-        indices.addAll(pm.getIndexList(pattern2, peptide));
+        for (Character amino : aa) {
+            for (int index = peptide.indexOf(amino); index >= 0 && index <= peptide.length() - 2;
+                    index = peptide.indexOf(amino, index + 1)) {
+                String p1 = peptide.substring(index, index + 1);
+                String p1F = peptide.substring(index + 1, index + 2);
+                if (index >= 1) {
+                    String p2 = peptide.substring(index - 1, index);
+                    if (amino.equals('K') && p2.equals("W") && p1F.equals("P")) {
+                        indices.add(index);
+                    } else if (amino.equals('R') && p2.equals("M") && p1F.equals("P")) {
+                        indices.add(index);
+                    } else {
+                        if (amino.equals('K') && p2.equals("C") && p1F.equals("D") || p1F.equals("H")
+                                || p1F.equals("Y")) {
+                        } else if (amino.equals('K') && p2.equals("D") && p1F.equals("D")) {
+                        } else if (amino.equals('R') && p2.equals("C") && p1F.equals("K")) {
+                        } else if (amino.equals('R') && p2.equals("R") && p1F.equals("R") || p1F.equals("H")) {
+                        } else if (!p1F.equals("P")) {
+                            indices.add(index);
+                        }
+                    }
+                } else {
+                    if (!p1F.equals('P')) {
+                        indices.add(index);
+                    }
+                }
+
+            }
+        }
         PeptideCutter pc = new PeptideCutter();
         return pc.getDigestionArray(peptide, indices, this.minimalLength);
     }
