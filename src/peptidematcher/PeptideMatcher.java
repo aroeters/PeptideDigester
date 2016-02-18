5d872c18aa1da2e512ab8199084fc2bdfec4f872
arne <arne@mw171175.med.rug.nl>
2016-02-18 11:13:19 +0100

    peptide matcher fixed

--- src/peptidematcher/PeptideMatcher.java
+++ src/peptidematcher/PeptideMatcher.java
@@ -14,12 +14,20 @@ import java.util.regex.Pattern;
  * @author arne
  */
 public class PeptideMatcher {
+    /**
+     * Matches the regex that is given and returns the array with indices.
+     * @param pattern compiled regex
+     * @param peptide peptide/protein to match on
+     * @return ArrayList<Integer>
+     */
     public ArrayList<Integer> getIndexList(final Pattern pattern, final String peptide) {
         ArrayList<Integer> indices = new ArrayList<>();
         Matcher matcher = pattern.matcher(peptide);
         while (matcher.find()) {
-            indices.add(matcher.start() + 1);
+            if (!indices.contains(matcher.start() + 1)) {
+                indices.add(matcher.start() + 1);
+            }
         }
-    return indices;
+        return indices;
     }
 }
