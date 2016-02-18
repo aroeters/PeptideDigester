2d14b0871fb0bfeeb2836e7c8a8ad0238cbaf14d
arne <arne@mw171175.med.rug.nl>
2016-02-18 13:01:14 +0100

    peptide matcher fixed

--- src/peptidedigesters/TrypsinDigester.java
+++ src/peptidedigesters/TrypsinDigester.java
@@ -37,42 +37,58 @@ public class TrypsinDigester implements Digester {
     public final ArrayList<String> digest(final String peptide) {
         // Trypsin
         // The order for the sites is:
-        // p4   p3  p2  p1  p1F p2F p3F p4F
+        // p4   p3  p2  p1/amino  p1F p2F p3F p4F
         indices = new ArrayList<>();
         this.indices.add(0);
         this.indices.add(peptide.length());
-        ArrayList<Character> aa = new ArrayList<>(Arrays.asList('K', 'R'));
-        for (Character amino : aa) {
+        ArrayList<String> aa = new ArrayList<>(Arrays.asList("K", "R"));
+        for (String amino : aa) {
             for (int index = peptide.indexOf(amino); index >= 0 && index <= peptide.length() - 2;
                     index = peptide.indexOf(amino, index + 1)) {
                 String p1 = peptide.substring(index, index + 1);
                 String p1F = peptide.substring(index + 1, index + 2);
                 if (index >= 1) {
                     String p2 = peptide.substring(index - 1, index);
-                    if (amino.equals('K') && p2.equals("W") && p1F.equals("P")) {
+                    if (p2.equals("W") && amino.equals("K") && p1F.equals("P")) {
                         indices.add(index);
-                    } else if (amino.equals('R') && p2.equals("M") && p1F.equals("P")) {
+                    } else if (p2.equals("M") && amino.equals("R") && p1F.equals("P")) {
                         indices.add(index);
                     } else {
-                        if (amino.equals('K') && p2.equals("C") && p1F.equals("D") || p1F.equals("H")
-                                || p1F.equals("Y")) {
-                        } else if (amino.equals('K') && p2.equals("D") && p1F.equals("D")) {
-                        } else if (amino.equals('R') && p2.equals("C") && p1F.equals("K")) {
-                        } else if (amino.equals('R') && p2.equals("R") && p1F.equals("R") || p1F.equals("H")) {
+                        if (p2.equals("C")) {
+                            if (amino.equals("K")) {
+                                if (p1F.equals("D") || p1F.equals("H") || p1F.equals("Y")) {
+                                    continue;
+                                } else if (!p1F.equals("P")) {
+                                    indices.add(index);
+                                }
+                            } else if (amino.equals("R")) {
+                                if (p1F.equals("K")) {
+                                    continue;
+                                } else if (!p1F.equals("P")) {
+                                    indices.add(index);
+                                }
+                            } else if (!p1F.equals("P")) {
+                                indices.add(index);
+                            }
+                        } else if (p2.equals("D") && amino.equals("K") && p1F.equals("D")) {
+                            continue;
+                        } else if (p2.equals("R") && amino.equals("R")) {
+                            if (p1F.equals("R") || p1F.equals("H")) {
+                                continue;
+                            }
                         } else if (!p1F.equals("P")) {
                             indices.add(index);
                         }
-                    }
-                } else {
-                    if (!p1F.equals('P')) {
-                        indices.add(index);
+
                     }
                 }
-
             }
+
         }
         PeptideCutter pc = new PeptideCutter();
-        return pc.getDigestionArray(peptide, indices, this.minimalLength);
+
+        return pc.getDigestionArray(peptide, indices,
+                this.minimalLength);
     }
 
     @Override
