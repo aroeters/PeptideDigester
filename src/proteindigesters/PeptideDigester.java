4ac95e4b77898451a9a14344db1bbb9ae806d09b
arne <arne@mw171175.med.rug.nl>
2016-05-25 14:22:51 +0200

    Main changed

--- src/proteindigesters/PeptideDigester.java
+++ src/proteindigesters/PeptideDigester.java
@@ -12,42 +12,16 @@ public class PeptideDigester {
      * @param args the command line arguments
      */
     public static void main(final String[] args) {
-        String peptideTRY = "HEQKQQVVKFLIKKKANLNALDRYGRTALILAVCCGSASIVNLLLEQNVDVSSQDLSGQTAREYAVSSHH";
-//        String peptideTRY2 = "GHYEVTGSDDETGKLQGSGVSLASKK";
-//        String peptideTRY3 = "GDVPSVGLEGPDVDLQGPEAK";
-//        String peptideTRY4 = "VEGTLKGPEVDLKGPR";
-//        String peptideTRY5 = "ADLDVSGPKVDIDVPDVNIEGPEGK";
+        String peptideTry = "VEGTLKGPEVDLKGPR";
         Integer i = 1;
         Integer count = 0;
         ArrayList<String> tryout = new ArrayList<>();
         while (count < i) {
              count++;
-             tryout.add(peptideTRY);
-//             tryout.add(peptideTRY2);
-//             tryout.add(peptideTRY3);
-//             tryout.add(peptideTRY4);
-//             tryout.add(peptideTRY5);
+             tryout.add(peptideTry);
         }
-//        System.out.println("Starting to process " + i*5 + " peptides to show the performance.");
-//        ChemotrypsinDigesterLowSpecific o1 = new ChemotrypsinDigesterLowSpecific(0, 3);
-//        long start1 = System.currentTimeMillis();
-//        for (String peptide : tryout) {
-//            System.out.println(o1.digest(peptide));
-//        }
-//        System.out.println("Chemotrypsin low spec\t" +(System.currentTimeMillis() - start1)/1000 + " s");
-//        ChemotrypsinDigesterHighSpecific o2 = new ChemotrypsinDigesterHighSpecific(0, 3);
-//        long start2 = System.currentTimeMillis();
-//        for (String peptide : tryout) {
-//            o2.digest(peptide);
-//        }
-//        System.out.println("Chemotrypsin high spec\t" +(System.currentTimeMillis() - start2)/1000 + " s");
-//        TrypsinDigester o3 = new TrypsinDigester(0, 3);
-//        long start3 = System.currentTimeMillis();
-//        for (String peptide : tryout) {
-//            o3.digest(peptide);
-//        }
-//        System.out.println("Trypsin\t\t\t" +(System.currentTimeMillis() - start3)/1000 + " s");
-        TrypsinDigesterConservative o4 = new TrypsinDigesterConservative(0, 3);
+
+        TrypsinDigester o4 = new TrypsinDigester(6, 3);
         long start4 = System.currentTimeMillis();
         for (String peptide : tryout) {
             System.out.println(o4.digest(peptide));
